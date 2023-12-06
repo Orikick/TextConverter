0f5dddc5cbe99288433e050d780d6142d77def21
Orikick <113384248+Orikick@users.noreply.github.com>
2023-12-06 15:39:12 +0200

    sec

---
+++ index.js
@@ -0,0 +1,388 @@
+
+const inputText = document.getElementById('inputText');
+const outputText = document.getElementById('outputText');
+const letterCount = document.getElementById('letterCount');
+const letterCountWithoutSpaces = document.getElementById('letterCountWithoutSpaces');
+const wordCount = document.getElementById('wordCount');
+const lineCount = document.getElementById('lineCount');
+const digitAndSymbolCount = document.getElementById('digitAndSymbolCount');
+inputText.addEventListener('input', updateCounters);
+
+function updateCounters() {
+    const text = inputText.value;
+    letterCount.textContent = text.length;
+    letterCountWithoutSpaces.textContent = text.replace(/\s/g, '').length;
+    wordCount.textContent = text.split(/\s+/).length;
+    lineCount.textContent = text.split('\n').length;
+    digitAndSymbolCount.textContent = countLatinDigitsAndSymbols(text);
+    letterCountWithoutSpecial.textContent = text.replace(/[0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/g, '').length;
+}
+
+
+function countLettersWithoutSpecial(text) {
+// Use a regular expression to match only letters (excluding special characters)
+const lettersWithoutSpecialRegex = /[a-zA-Z]/g;
+const matches = text.match(lettersWithoutSpecialRegex);
+return matches ? matches.length : 0;
+}
+function countLatinDigitsAndSymbols(text) {
+// Use a regular expression to match Latin letters, digits, and special characters
+const latinDigitsAndSymbolsRegex = /[0-9!@#$%^&*()_+{}\[\]:;<>,.?~\\/-]/g;
+const matches = text.match(latinDigitsAndSymbolsRegex);
+return matches ? matches.length : 0;
+}
+
+function convert() {
+    const conversionType = document.getElementById('conversionType').value;
+    const inputText = document.getElementById('inputText').value;
+
+    switch (conversionType) {
+        case 'morse':
+            document.getElementById('outputText').value = textToMorse(inputText);
+            break;
+        case 'transliterate':
+            document.getElementById('outputText').value = transliterateText(inputText);
+            break;
+        case 'nato':
+            document.getElementById('outputText').value = textToNato(inputText);
+            break;
+        case 'toupper':
+            document.getElementById('outputText').value = convertUpperCase(inputText);
+            break;
+        case 'tolower':
+            document.getElementById('outputText').value = convertLowerCase(inputText);
+            break;
+        case 'toupperlower':
+            document.getElementById('outputText').value = convertUpperLowerCase(inputText);
+            break;
+        case 'clearhtml':
+            document.getElementById('outputText').value = clearHtmlTags(inputText);
+            break;
+            case 'transgerman':
+            document.getElementById('outputText').value = transliterateGermanToUkrainian(inputText);
+            break;
+            case 'transpolish':
+            document.getElementById('outputText').value = transliteratePolishToUkrainian(inputText);
+            break;
+            
+            
+        default:
+            break;
+    }
+}
+
+function copyOutput() {
+    const outputText = document.getElementById('outputText');
+    outputText.select();
+    document.execCommand('copy');
+    alert('Результат скопійовано до буферу обміну!');
+}
+
+
+
+function convertToMorseCode() {
+    const inputText = document.getElementById('inputText').value.toUpperCase();
+    const morseCode = textToMorse(inputText);
+    document.getElementById('outputText').value = morseCode;
+}
+
+function transliterate() {
+    const inputText = document.getElementById('inputText').value;
+    const transliteratedText = transliterateText(inputText);
+    document.getElementById('outputText').value = transliteratedText;
+}
+
+function transliterateGerman() {
+    const inputText = document.getElementById('inputText').value;
+    const transliteratedText = transliterateGermanToUkrainian(inputText);
+    document.getElementById('outputText').value = transliteratedText;
+}
+
+function transliteratePolish() {
+    const inputText = document.getElementById('inputText').value;
+    const transliteratedText = transliteratePolishToUkrainian(inputText);
+    document.getElementById('outputText').value = transliteratedText;
+}
+
+function encodeNato() {
+    const inputText = document.getElementById('inputText').value.toUpperCase();
+    const natoCode = textToNato(inputText);
+    document.getElementById('outputText').value = natoCode;
+}
+
+
+function ToUpperCase() {
+    const inputText = document.getElementById('inputText').value.toUpperCase();
+    const uppercase = convertUpperCase(inputText);
+    document.getElementById('outputText').value = uppercase;
+}
+function ToLowerCase() {
+    const inputText = document.getElementById('inputText').value.toUpperCase();
+    const lowerCase = convertLowerCase(inputText);
+    document.getElementById('outputText').value = lowerCase;
+}
+
+function ToUpperLowerCase() {
+    const inputText = document.getElementById('inputText').value.toUpperCase();
+    const upperlowerCase = onvertUpperLowerCase(inputText);
+    document.getElementById('outputText').value = upperlowerCase;
+}
+
+function ToclearHtmlTags() {
+    const inputText = document.getElementById('inputText').value.toUpperCase();
+    const clearHtml = clearHtmlTags(inputText);
+    document.getElementById('outputText').value = clearHtml;
+}
+
+function clearHtmlTags(text) {
+    return text.replace(/<[^>]+>/g, '');
+}
+function convertUpperCase(text) {
+    return text.toUpperCase();
+}
+function convertLowerCase(text) {
+    return text.toLowerCase();
+}
+
+function convertUpperLowerCase(text) {
+    var result = "";
+
+    for (var i = 0; i < text.length; i++) {
+        if (i % 2 === 0) {
+            result += text[i].toUpperCase();
+        } else {
+            result += text[i].toLowerCase();
+        }
+    }
+
+    return result;
+}
+function textToMorse(text) {
+    const morseCodeMap = {
+        'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....', 'I': '..', 'J': '.---',
+        'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.', 'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-',
+        'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-', 'Y': '-.--', 'Z': '--..',
+        '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....', '7': '--...', '8': '---..', '9': '----.',
+        '.': '.-.-.-', ',': '--..--', '?': '..--..', "'": '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.', ')': '-.--.-', '&': '.-...',
+        ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-', '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.'
+    };
+
+    text = text.toUpperCase();
+    let morseCode = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        if (char === ' ') {
+            morseCode += '   ';  // Вставляємо три пробіли між словами
+        } else if (morseCodeMap[char]) {
+            morseCode += morseCodeMap[char] + ' ';
+        } else {
+            morseCode += char;  // Залишаємо символи, які не входять до азбуки Морзе, незмінними
+        }
+    }
+
+    return morseCode.trim();
+}
+
+
+function transliterateText(text) {
+    const transliterationMap = {
+        'а': 'a', 'б': 'b', 'в': 'v', 'г': 'g', 'ґ': 'g', 'д': 'd', 'е': 'e', 'є': 'ie', 'ж': 'zh', 'з': 'z',
+        'и': 'i', 'і': 'i', 'ї': 'i', 'й': 'i', 'к': 'k', 'л': 'l', 'м': 'm', 'н': 'n', 'о': 'o', 'п': 'p',
+        'р': 'r', 'с': 's', 'т': 't', 'у': 'u', 'ф': 'f', 'х': 'kh', 'ц': 'ts', 'ч': 'ch', 'ш': 'sh', 'щ': 'shch',
+        'ю': 'iu', 'я': 'ia', 'зг': 'zgh',
+        'А': 'A', 'Б': 'B', 'В': 'V', 'Г': 'G', 'Ґ': 'G', 'Д': 'D', 'Е': 'E', 'Є': 'Ye', 'Ж': 'Zh', 'З': 'Z',
+        'И': 'I', 'І': 'I', 'Ї': 'I', 'Й': 'I', 'К': 'K', 'Л': 'L', 'М': 'M', 'Н': 'N', 'О': 'O', 'П': 'P',
+        'Р': 'R', 'С': 'S', 'Т': 'T', 'У': 'U', 'Ф': 'F', 'Х': 'Kh', 'Ц': 'Ts', 'Ч': 'Ch', 'Ш': 'Sh', 'Щ': 'Shch',
+        'Ь': '', 'Ю': 'Iu', 'Я': 'Ia'
+    };
+
+    let transliteratedText = '';
+    let capitalizeNext = true;
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        if (char === '’') {
+            continue;
+        }
+        if (char === 'ь') {
+            continue;
+        }
+        // Check for "зг" and transliterate accordingly
+        if (i < text.length - 1 && char === 'з' && text[i + 1] === 'г') {
+            transliteratedText += transliterationMap['зг'];
+            i++; // Skip the next character ('г')
+        } else if (transliterationMap[char]) {
+            // Check if it's the first letter of a word
+            if (capitalizeNext) {
+                // Apply the specific rule for the first letter
+                switch (char) {
+                    case 'я':
+                        transliteratedText += 'ya';
+                        break;
+                    case 'є':
+                        transliteratedText += 'ye';
+                        break;
+                    case 'ї':
+                        transliteratedText += 'yi';
+                        break;
+                    case 'й':
+                        transliteratedText += 'Y';
+                        break;
+                    case 'ю':
+                        transliteratedText += 'yu';
+                        break;
+                    default:
+                        // Use the general transliteration rule
+                        transliteratedText += transliterationMap[char];
+                        break;
+                }
+                capitalizeNext = false;
+            } else {
+                // Use the general transliteration rule
+                transliteratedText += transliterationMap[char];
+            }
+        } else {
+            // Keep characters that do not undergo transliteration unchanged
+            transliteratedText += char;
+            // Set the flag to capitalize the next letter if the current character is a space
+            capitalizeNext = char === ' ';
+        }
+    }
+
+    return transliteratedText;
+}
+function transliterateGermanToUkrainian(text) {
+    const transliterationMap = {
+'a': 'а', 'aa': 'а', 'ah': 'а', 'аі': 'ай', 'au': 'ау', 'ä': 'е', 'e': 'е', 'ee': 'е', 'eh': 'е', 'ei': 'ай',
+'eu': 'ой', 'f': 'ф', 'g': 'г/ґ', 'h': 'г', 'i': 'й', 'і': 'й', 'ie': 'і/и', 'ih': 'і/и', 'j': 'й', 'k': 'к',
+'l': 'л', 'ль': 'ль', 'm': 'м', 'n': 'н', 'ng': 'нг', 'nk': 'нк', 'o': 'о', 'oh': 'о', 'oo': 'о', 'ö': 'е',
+'p': 'п', 'ph': 'ф', 'qu': 'кв', 'r': 'р', 's': 'ш', 'з': 'ш', 'с': 'с', 'sch': 'ш', 'ss': 'с', 'ß': 'с',
+'t': 'т', 'th': 'т', 'tsch': 'ч', 'tz': 'ц', 'u': 'у', 'uh': 'у', 'ü': 'ю', 'üh': 'ю, і', 'v': 'в', 'ф': 'ф',
+'w': 'в', 'x': 'кс', 'y': 'і/и', 'й': 'й', 'z': 'ц'
+};
+
+let transliteratedText = '';
+let currentChunk = '';
+
+for (let i = 0; i < text.length; i++) {
+const currentChar = text[i];
+const nextChar = text[i + 1];
+const currentChunkPlusNextChar = currentChunk + nextChar;
+
+if (transliterationMap[currentChunkPlusNextChar]) {
+    currentChunk = currentChunkPlusNextChar;
+    i++; // Skip the next character
+} else if (transliterationMap[currentChunk]) {
+    transliteratedText += transliterationMap[currentChunk];
+    currentChunk = currentChar;
+} else {
+    currentChunk += currentChar;
+}
+}
+
+if (transliterationMap[currentChunk]) {
+transliteratedText += transliterationMap[currentChunk];
+} else {
+transliteratedText += currentChunk;
+}
+
+return transliteratedText;
+}
+function transliteratePolishToUkrainian(text) {
+const transliterationMap = {
+'a': 'а', 'ą': 'он',
+'b': 'б', 'c': 'ц', 'ch': 'х', 'cz': 'ч', 'ć': 'ць',
+'d': 'д', 'dz': 'дз', 'dź': 'дзь', 'dż': 'дж',
+'e': 'е', 'ę': 'ен',
+'f': 'ф',
+'g': 'г', 'ґ': 'ґ', 'h': 'г',
+'i': 'і', 'ia': 'я', 'ią': 'ьон', 'ie': 'е', 'ię': 'єн', 'io': 'йо', 'ió': 'ю', 'iu': 'ю',
+'j': 'й', 'ja': 'я', 'ją': 'йон', 'je': 'є', 'ję': 'єн', 'jo': 'йо', 'jó': 'ю', 'ju': 'ю',
+'k': 'к',
+'l': 'л', 'ł': 'л',
+'m': 'м', 'n': 'н', 'ń': 'нь',
+'o': 'о', 'ó': 'у',
+'p': 'п',
+'r': 'р', 'rz': 'ж',
+'s': 'с', 'sz': 'ш', 'szcz': 'щ', 'ś': 'сь',
+'t': 'т',
+'u': 'у',
+'w': 'в',
+'y': 'и',
+'z': 'з', 'ź': 'Зь', 'ż': 'ж',
+'A': 'А', 'Ą': 'Он',
+'B': 'Б', 'C': 'Ц', 'Ch': 'Х', 'Cz': 'Ч', 'Ć': 'Ць',
+'D': 'Д', 'Dz': 'Дз', 'Dź': 'Дзь', 'Dż': 'Дж',
+'E': 'Е', 'Ę': 'Ен',
+'F': 'Ф',
+'G': 'Г', 'Ґ': 'Ґ', 'H': 'Г',
+'I': 'І', 'Ia': 'Я', 'Ią': 'Йон', 'Ie': 'Е', 'Ię': 'Єн', 'Io': 'Йо', 'Ió': 'Ю', 'Iu': 'Ю',
+'J': 'Й', 'Ja': 'Я', 'Ją': 'Йон', 'Je': 'Є', 'Ję': 'Єн', 'Jo': 'Йо', 'Jó': 'Ю', 'Ju': 'Ю',
+'K': 'К',
+'L': 'Л', 'Ł': 'Л',
+'M': 'М', 'N': 'Н', 'Ń': 'Нь',
+'O': 'О', 'Ó': 'У',
+'P': 'П',
+'R': 'Р', 'Rz': 'Ж',
+'S': 'С', 'Sz': 'Ш', 'Szcz': 'Щ', 'Ś': 'Сь',
+'T': 'Т',
+'U': 'У',
+'W': 'В',
+'Y': 'И',
+'Z': 'З', 'Ź': 'Зь', 'Ż': 'Ж'
+};
+
+let result = '';
+let i = 0;
+
+while (i < text.length) {
+let currentChar = text[i];
+let nextChar = text.slice(i, i + 2);
+
+if (transliterationMap[nextChar]) {
+    result += transliterationMap[nextChar];
+    i += 2;
+} else if (transliterationMap[currentChar]) {
+    result += transliterationMap[currentChar];
+    i += 1;
+} else {
+    result += currentChar;
+    i += 1;
+}
+}
+
+return result;
+}
+
+
+
+
+function textToNato(text) {
+    const natoAlphabet = {
+        'A': 'Alpha', 'B': 'Bravo', 'C': 'Charlie', 'D': 'Delta', 'E': 'Echo', 'F': 'Foxtrot', 'G': 'Golf',
+        'H': 'Hotel', 'I': 'India', 'J': 'Juliett', 'K': 'Kilo', 'L': 'Lima', 'M': 'Mike', 'N': 'November',
+        'O': 'Oscar', 'P': 'Papa', 'Q': 'Quebec', 'R': 'Romeo', 'S': 'Sierra', 'T': 'Tango', 'U': 'Uniform',
+        'V': 'Victor', 'W': 'Whiskey', 'X': 'X-ray', 'Y': 'Yankee', 'Z': 'Zulu',
+        '0': 'Zero', '1': 'One', '2': 'Two', '3': 'Three', '4': 'Four', '5': 'Five', '6': 'Six', '7': 'Seven', '8': 'Eight', '9': 'Nine',
+        '.': 'Decimal Point', ',': 'Comma', '?': 'Question Mark', "'": 'Apostrophe', '!': 'Exclamation Mark', '/': 'Slash', '(': 'Left Parenthesis',
+        ')': 'Right Parenthesis', '&': 'Ampersand', ':': 'Colon', ';': 'Semicolon', '=': 'Equals Sign', '+': 'Plus Sign', '-': 'Hyphen', '_': 'Underscore',
+        '"': 'Double Quote', '$': 'Dollar Sign', '@': 'At Sign'
+    };
+
+    text = text.toUpperCase();
+    let natoCode = '';
+
+    for (let i = 0; i < text.length; i++) {
+        const char = text[i];
+        if (char === ' ') {
+            natoCode += ' ';  // Пробіл залишається незмінним
+        } else if (natoAlphabet[char]) {
+            natoCode += natoAlphabet[char] + ' ';
+        } else {
+            natoCode += char;  // Залишаємо символи, які не входять до шифру НАТО, незмінними
+        }
+    }
+
+    return natoCode.trim();
+}
